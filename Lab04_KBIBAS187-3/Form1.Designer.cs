bbfceb83a492ac9c62ef723d36ab7db590ea3e38
Эдуард Слончак <stud0000210513@study.utmn.ru>
2019-11-03 16:09:08 +0500

    New

--- Lab04_KBIBAS187-3/Form1.Designer.cs
+++ Lab04_KBIBAS187-3/Form1.Designer.cs
@@ -31,6 +31,12 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,17 +61,21 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Location = new System.Drawing.Point(12, 19);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(85, 17);
             this.radioButton1.TabIndex = 1;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "radioButton1";
             this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
             this.groupBox1.Location = new System.Drawing.Point(139, 12);
             this.groupBox1.Name = "groupBox1";
@@ -74,6 +84,62 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(109, 65);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 6;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(109, 42);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(109, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -81,11 +147,13 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.comboBox1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,6 +162,12 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Lab04_KBIBAS187-3/Form1.cs
+++ Lab04_KBIBAS187-3/Form1.cs
@@ -13,6 +13,8 @@ namespace Lab04_KBIBAS187_3
     public partial class Form1 : Form
     {
         //public Boolean args { get; set; }
+        public Int32 Index { get; set; } = 0;
+        //public  SystemMenu
         public Font[] Fonts { get; } =
         {
             new Font("Times New Roman", 8), new Font("Trebuchet MS", 8), new Font("Microsoft Tai Le", 8),
@@ -22,44 +24,69 @@ namespace Lab04_KBIBAS187_3
         public Form1()
         {
             InitializeComponent();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(356, 31);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.Controls.Add(this.checkBox1);
+            checkBox1.CheckedChanged += CheckBox1_CheckChanged;
         }
 
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             String selectedstring = comboBox1.Text;
-            switch (selectedstring)
+            foreach (Control control in Controls)
             {
-                case "Times New Roman":
-                    Font = Fonts[0];
-                    break;
-                case "Trebuchet MS":
-                    Font = Fonts[1];
-                    break;
-                case "Microsoft Tai Le":
-                    Font = Fonts[2];
-                    break;
-                case "Regurgitation":
-                    Font = Fonts[3];
-                    break;
-                case "Yu Gothic":
-                    Font = Fonts[4];
-                    break;
-                case "Arial":
-                    Font = Fonts[5];
-                    break;
-                case "Microsoft Sans Serif":
-                    Font = Fonts[6];
-                    break;
-                case "Javanese Text":
-                    Font = Fonts[7];
-                    break;
-                //case
+                switch (selectedstring)
+                {
+                    //Font.Style = FontStyle.Regular;
+                    case "Times New Roman":
+                        control.Font = Fonts[0];
+                        break;
+                    case "Trebuchet MS":
+                        control.Font = Fonts[1];
+                        break;
+                    case "Microsoft Tai Le":
+                        control.Font = Fonts[2];
+                        break;
+                    case "Regurgitation":
+                        control.Font = Fonts[3];
+                        break;
+                    case "Yu Gothic":
+                        control.Font = Fonts[4];
+                        break;
+                    case "Arial":
+                        control.Font = Fonts[5];
+                        break;
+                    case "Microsoft Sans Serif":
+                        control.Font = Fonts[6];
+                        break;
+                    case "Javanese Text":
+                        control.Font = Fonts[7];
+                        break;
+                    //case
+                }
             }
         }
 
-        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        private void CheckBox1_CheckChanged(object sender, EventArgs e)
         {
-
+            CheckBox checkBox = (CheckBox) sender;
+            if (checkBox.Checked)
+            {
+                Text = "";
+            }
+            else
+            {
+                Text = "Form1";
+            }
         }
     }
 }
