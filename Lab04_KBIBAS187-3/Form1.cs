f25bf210a81687108e8d7f4c5319dbab1a8c6f2a
Эдуард Слончак <stud0000210513@study.utmn.ru>
2019-11-03 21:42:12 +0500

    Финал очка

--- Lab04_KBIBAS187-3/Form1.cs
+++ Lab04_KBIBAS187-3/Form1.cs
@@ -1,28 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab04_KBIBAS187_3
 {
     public partial class Form1 : Form
     {
-        //public Boolean args { get; set; }
-        public Int32 Index { get; set; } = 0;
-
-        //public  SystemMenu
-        public Font[] Fonts { get; } =
-        {
-            new Font("Times New Roman", 8), new Font("Trebuchet MS", 8), new Font("Microsoft Tai Le", 8),
-            new Font("Regurgitation", 8), new Font("Yu Gothic", 8), new Font("Arial", 8),
-            new Font("Microsoft Sans Serif", 8), new Font("Javanese Text", 8)
-        };
-
         public Form1()
         {
             InitializeComponent();
@@ -36,26 +19,36 @@ namespace Lab04_KBIBAS187_3
             checkBox2.UseVisualStyleBackColor = true;
             Controls.Add(checkBox2);
             checkBox2.CheckStateChanged += CheckBox2_CheckChanged;
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            checkBox1 = new CheckBox();
             // 
             // checkBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(356, 31);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.Controls.Add(this.checkBox1);
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(356, 31);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(80, 17);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            Controls.Add(checkBox1);
             checkBox1.CheckedChanged += CheckBox1_CheckChanged;
         }
 
+        //public Boolean args { get; set; }
+        public int Index { get; set; } = 0;
+
+        //public  SystemMenu
+        public Font[] Fonts { get; } =
+        {
+            new Font("Times New Roman", 8), new Font("Trebuchet MS", 8), new Font("Microsoft Tai Le", 8),
+            new Font("Regurgitation", 8), new Font("Yu Gothic", 8), new Font("Arial", 8),
+            new Font("Microsoft Sans Serif", 8), new Font("Javanese Text", 8)
+        };
+
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            String selectedstring = comboBox1.Text;
+            var selectedstring = comboBox1.Text;
             foreach (Control control in Controls)
-            {
                 switch (selectedstring)
                 {
                     //Font.Style = FontStyle.Regular;
@@ -85,7 +78,6 @@ namespace Lab04_KBIBAS187_3
                         break;
                     //case
                 }
-            }
         }
 
         private void CheckBox2_CheckChanged(object sender, EventArgs e)
@@ -96,65 +88,53 @@ namespace Lab04_KBIBAS187_3
 
         private void CheckBox1_CheckChanged(object sender, EventArgs e)
         {
-            CheckBox checkBox = (CheckBox) sender;
-            if (checkBox.Checked)
-            {
-                Text = "";
-            }
-            else
-            {
-                Text = "Form1";
-            }
+            var checkBox = (CheckBox) sender;
+            if (checkBox.Checked) Text = "";
+            else Text = "Form1";
         }
 
         private void ToolStripMenuItem5_Click(object sender, EventArgs e)
         {
-            if (listBox1.Items.Count!=0) listBox1.Items.Remove(listBox1.SelectedItem);
+            if (listBox1.Items.Count != 0) listBox1.Items.Remove(listBox1.SelectedItem);
         }
 
         private void ToolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
         {
             //Text = "";
-            if (e.KeyCode==Keys.Enter)
-            {
-                if (toolStripTextBox1.TextLength!=0)
+            if (e.KeyCode == Keys.Enter)
+                if (toolStripTextBox1.TextLength != 0)
                 {
                     listBox1.Items.Add(toolStripTextBox1.Text);
                     toolStripTextBox1.Text = "";
                 }
-            }
         }
 
         private void ToolStripTextBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
-            {
-                e.Handled = true;
-            }
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8)) e.Handled = true;
         }
 
         private void ToolStripMenuItem6_Click(object sender, EventArgs e)
         {
-            Int32 a = 0;
-            Int32 count = 0;
-            Int32 sum = 0;
+            var a = 0;
+            var count = 0;
+            var sum = 0;
             foreach (var item in listBox1.Items)
             {
                 a = int.Parse(item.ToString());
-                if (a%2==0)
+                if (a % 2 == 0)
                 {
                     count++;
                     sum += a;
                 }
             }
 
-            if (count>0)
+            if (count > 0)
             {
-
                 listBox2.Items.Add($"Сумма:{sum}");
                 listBox2.Items.Add($"Чётных {count}");
             }
-            else if (listBox1.Items.Count!=0)
+            else if (listBox1.Items.Count != 0)
             {
                 listBox2.Items.Add("Чётных нет, но вы держитесь.");
             }
@@ -162,39 +142,29 @@ namespace Lab04_KBIBAS187_3
             {
                 listBox2.Items.Add("Может в ListBox что-то добавить?");
             }
-
         }
 
         private void ToolStripMenuItem7_Click(object sender, EventArgs e)
         {
-            Int32 integer;
-            Boolean evencount = false;
-            Boolean reseti = false;
+            int integer;
+            var evencount = false;
+            var reseti = false;
             if (listBox1.Items.Count >= 5)
             {
-                for (int i = listBox1.Items.Count - 1; i>=listBox1.Items.Count-5; i--)
+                for (var i = listBox1.Items.Count - 1; i >= listBox1.Items.Count - 5; i--)
                 {
-                    int.TryParse(listBox1.Items[i].ToString(),out integer);
-                    if (integer % 2 == 0&&!evencount)
-                    {
-                        evencount = true;
-                    }
-
-                    if (evencount&&!reseti)
+                    int.TryParse(listBox1.Items[i].ToString(), out integer);
+                    if (integer % 2 == 0 && !evencount) evencount = true;
+                    if (evencount && !reseti)
                     {
                         i = listBox1.Items.Count - 1;
                         reseti = true;
                     }
-                    if (evencount&&reseti)
-                    {
-                        listBox2.Items.Add(listBox1.Items[i]);
-                    }
+
+                    if (evencount && reseti) listBox2.Items.Add(listBox1.Items[i]);
                 }
 
-                if (!evencount)
-                {
-                    listBox2.Items.Add("Среди 5-ти чисел нет ни одного чётного");
-                }
+                if (!evencount) listBox2.Items.Add("Среди 5-ти чисел нет ни одного чётного");
             }
             else
             {
@@ -204,48 +174,34 @@ namespace Lab04_KBIBAS187_3
 
         private void ToolStripTextBox2_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode==Keys.Enter)
-            {
-                if (toolStripTextBox2.TextLength!=0)
-                {
+            if (e.KeyCode == Keys.Enter)
+                if (toolStripTextBox2.TextLength != 0)
                     listBox3.Items.Add(toolStripTextBox2.Text);
-                }
-            }
         }
 
         private void ToolStripMenuItem8_Click(object sender, EventArgs e)
         {
-            Boolean str = false;
-            foreach (String striItem in listBox3.Items)
-            {
-                if (striItem.ToUpper()[striItem.Length-1] >= 'А' && striItem.ToUpper()[striItem.Length - 1] <= 'Я')
+            var str = false;
+            foreach (string striItem in listBox3.Items)
+                if (striItem.ToUpper()[striItem.Length - 1] >= 'А' && striItem.ToUpper()[striItem.Length - 1] <= 'Я')
                 {
                     listBox2.Items.Add(striItem);
                     str = true;
                 }
-            }
 
-            if (!str)
-            {
-                listBox2.Items.Add("Нет таких строк");
-            }
+            if (!str) listBox2.Items.Add("Нет таких строк");
         }
 
         private void ToolStripMenuItem9_Click(object sender, EventArgs e)
         {
-            if (listBox3.Items.Count >=2)
+            if (listBox3.Items.Count >= 2)
             {
-                for (int i = 1; i < listBox3.Items.Count; i=+2)
-                {
-                   listBox2.Items.Add(listBox3.Items[i].ToString().Replace(" ", "  "));
-                }
-
-                for (int i = 0; i < listBox3.Items.Count; i+=2)
-                {
-                   listBox2.Items.Add (listBox3.Items[i].ToString().Replace(" ", ""));
-                }
+                for (var i = 1; i < listBox3.Items.Count; i = +2)
+                    listBox2.Items.Add(listBox3.Items[i].ToString().Replace(" ", "  "));
+                for (var i = 0; i < listBox3.Items.Count; i += 2)
+                    listBox2.Items.Add(listBox3.Items[i].ToString().Replace(" ", ""));
             }
-            else if (listBox3.Items.Count==1)
+            else if (listBox3.Items.Count == 1)
             {
                 listBox2.Items.Add(listBox3.Items[0].ToString().Replace(" ", ""));
             }
