5161bca8a32e8f3333d1f3647c066e2d675951e3
Эдуард Слончак <stud0000210513@study.utmn.ru>
2019-11-28 22:14:03 +0500

    Добавьте файлы проекта.

---
+++ Lab05_2_KBIBAS187-3/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab05_2_KBIBAS187_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Lab05_2_KBIBAS187-3/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab05_2_KBIBAS187_3
+{
+    class Student
+    {
+        private static Int32 StudentsFrom1Course(ref ListBox list, Int32 course)
+        {
+            Int32 count = 0;
+            foreach (Student student in list.Items)
+            {
+                if (student.Course == course)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public String Name { get; set; }
+        public String Specialization { get; set; }
+
+        public int Course
+        {
+            get
+            {
+                if (_course != 0)
+                {
+                    return _course;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Ошибка");
+                }
+            }
+            set
+            {
+                if (value > 0 && value <= 10)
+                {
+                    _course = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Сущетвует ли направление на которое учятся более 10 лет?");
+                }
+            }
+        }
+
+        public DateTime BirthDateTime { get; set; }
+        public String PlaceOfBirth { get; set; }
+
+        private Int32 _course;
+    }
+}
